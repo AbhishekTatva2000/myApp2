2708379f397ce9b41dcef053019f2a9b2c476d24
Abhishek Dave <tatva_pcs208@outlook.com>
2022-04-18 18:26:47 +0530

    myApp2 final commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root "home#index"
+  get "homepage", to: "home#index"
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
